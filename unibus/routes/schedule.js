030ccb0cd559daae8928742ecc255afb6c04be2a
brian <brianjune@naver.com>
2019-12-08 23:07:12 +0900

    warning 추가, calendar 손대다가 포기

--- unibus/routes/schedule.js
+++ unibus/routes/schedule.js
@@ -7,11 +7,23 @@ function authCheck(req, res, callback){
   if(req.user.type=="Student") callback(req,res,req.user);
 }
 
+//appointment를 생성하자.
 router.get('/create', (req,res)=>{
   authCheck(req,res,(req,res,user)=>{
-    if(!user.isLeader) return res.redirect('/main');
+    if(!user.isLeader) return res.render('student/warning');
+    else res.render('student/schedule_create',{
+      user:user
+    });
     
   })
 });
+
+//일정을 입력하자.
+router.get('/input', (req,res)=>{
+  authCheck(req,res,(req,res,user)=>{
+    if(user.team)
+
+  })
+})
 
 module.exports = router;
